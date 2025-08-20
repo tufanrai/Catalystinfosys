c3b5ab55eaa14eae71b46754a7b43c68c6565aac
TufanRai <thulungtufan16@gmail.com>
2025-08-20 11:47:51 +0545

    updated the team component

--- Component/Hero/banner.tsx
+++ Component/Hero/banner.tsx
@@ -5,10 +5,10 @@ const Banner = () => {
   return (
     <WavyBackground className="w-full h-auto flex justify-center items-center">
       <div className="max-w-[1280px] w-full h-auto flex flex-col items-center overflow-hidden">
-        <h1 className="text-center text-white/90 font-bold text-2xl md:text-[60px]">
+        <h1 className="text-center text-white/90 font-bold text-5xl md:text-[60px]">
           Catalystinfosys
         </h1>
-        <h6 className="text-center text-white font-medium text-[8px] md:text-lg">
+        <h6 className="text-center text-white font-medium text-sm mt-2 md:text-lg">
           The Digital Leap: From Local to Global
         </h6>
       </div>
--- Component/Hero/team.tsx
+++ Component/Hero/team.tsx
@@ -6,21 +6,34 @@ import { AboutBannerText } from "../contents/AboutUs";
 
 const Team = () => {
   return (
-    <div className="bg-black/90 text-white w-full h-auto">
-      <div className="max-w-[1280px] max-h-[70%] h-screen w-full flex flex-col md:flex-row items-center justify-evenly border md:justify-center gap-4 md:gap-8 overflow-hidden py-8 md:px-16">
-        <div className="max-w-[80%] max-h-[90%] w-full h-screen flex items-center justify-center border">
+    <div className="bg-black/90 w-full">
+      <div className="max-w-[1280px] h-screen w-full flex flex-col md:flex-row items-center justify-evenly md:justify-center md:gap-8 overflow-hidden md:px-16">
+        <div className="max-w-[80%] max-h-[70%] w-full h-screen flex flex-col items-center justify-center">
+          <span className="px-4 py-2 font-medium text-sm md:text-lg italic text-white/60 border border-blue-800 backdrop-blur-sm bg-blue-600/20 shadow-xl/30 shadow-blue-600/90 rounded-lg z-10 relative top-6 left-18">
+            Passionate team
+          </span>
           <AboutUsImgCard
             image={team1.src}
             className="max-w-60 max-h-40"
-            position="relative left-25"
+            position=""
           />
           <AboutUsImgCard
             image={team2.src}
             className="max-w-46 max-h-80 relative"
-            position="relative right-12 top-12 z-999"
+            position="relative bottom-12 z-10 right-12"
           />
         </div>
-        <div className=" max-w-[80%] max-h-[90%] w-full h-screen"></div>
+        <div className=" max-w-[80%] max-h-[30%] w-full flex flex-col items-center justify-start gap-1 mb-18 py-4">
+          <h1 className="text-start w-full font-bold text-lg md:text-2xl italic text-blue-700">
+            {AboutBannerText.title}
+          </h1>
+          <p className="text-start w-full font-light text-sm md:text-lg text-neutral-500">
+            {AboutBannerText.description}
+          </p>
+          <button className="px-5 py-2 font-medium text-sm md:text-md border border-blue-800 rounded-lg text-blue-800 bg-blue-800/20 mt-8">
+            About Us
+          </button>
+        </div>
       </div>
     </div>
   );
