21e7486fe49bfc80868950b6d2dd52710d1a6e66
TufanRai <thulungtufan16@gmail.com>
2025-09-04 16:29:57 +0545

    updated the padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
-        <div className="w-full flex flex-col justify-center bg-black md:px-16 px-8">
+        <div className="w-full flex flex-col justify-center bg-black">
           <HeaderCard />
           {children}
           <FooterPage />
